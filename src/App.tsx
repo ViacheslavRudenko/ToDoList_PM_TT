ea509bb184ed2aaa8c0b7b3e88bcbef9addcd0d5
ViacheslavRudenko <slavikrudenko@gmail.com>
2023-03-01 12:31:41 +0200

    feat: ToDoList component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
-import CreateToDo from "./Components/CreateToDo";
+import CreateToDo from "./Components/CreateToDo/CreateToDo";
+import ToDoList from "./Components/ToDo/ToDoList/ToDoList";
 
 function App() {
   return (
     <div className="App">
       <CreateToDo />
+      <ToDoList />
     </div>
   );
 }
---
+++ src/Components/ToDo/ToDoList/ToDoList.tsx
@@ -0,0 +1,17 @@
+import "./index.css";
+
+const ToDoList = () => {
+  return (
+    <div className="container">
+      <div className="title">
+        <p>ID</p>
+        <p>Title</p>
+        <p>Description</p>
+        <p>Status</p>
+      </div>
+      <div className="content"></div>
+    </div>
+  );
+};
+
+export default ToDoList;
